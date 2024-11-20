abb2ae24814e6d9498b8a9291cef934f4d7f4138
Aaron John-Baptiste <gravityapple@gmail.com>
2024-11-20 16:39:33 +0000

    applied prettier formatting

--- blueprint-server.js
+++ blueprint-server.js
@@ -5,62 +5,63 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const readFile = (filename) => {
-   const filePath = path.join(__dirname, filename);
-   const content = fs.readFileSync(filePath, 'utf8')
-      .replace(/^<\?php\s+/, '')
-      .replace(/^\s+/, '');
-   return content;
+const readFile = filename => {
+	const filePath = path.join(__dirname, filename);
+	const content = fs
+		.readFileSync(filePath, 'utf8')
+		.replace(/^<\?php\s+/, '')
+		.replace(/^\s+/, '');
+	return content;
 };
 
 export const generateBlueprint = (consumer, blogData, themeHtml, defaultParams) => {
-   return {
-      "$schema": "https://playground.wordpress.net/blueprint-schema.json",
-      "preferredVersions": {
-         "php": "8.3",
-         "wp": "latest"
-      },
-      "features": {
-         "networking": true
-      },
-      "landingPage": "/",
-      "login": true,
-      "steps": [
-         {
-            "step": "mkdir",
-            "path": "wordpress/wp-content/mu-plugins"
-         },
-         {
-            "step": "writeFile",
-            "path": "wordpress/wp-content/mu-plugins/addFilter-0.php",
-            "data": "<?php add_action( 'requests-requests.before_request', function( &$url ) {\n$url = 'https://playground.wordpress.net/cors-proxy.php?' . $url;\n} );"
-         },
-         {
-            "step": "installPlugin",
-            "pluginData": {
-               "resource": "url",
-               "url": "https://github-proxy.com/proxy/?repo=Automattic/tumblr-theme-translator&release=v0.1.3&asset=tumblr-theme-translator.zip"
-            }
-         },
-         {
-            "step": "login",
-            "username": "admin",
-            "password": "password"
-         },
-         {
-            "step": "setSiteOptions",
-            "options": {
-               "template": "tumblr3",
-               "stylesheet": "tumblr3"
-            }
-         },
-         {
-            "step": "wp-cli",
-            "command": `wp media import '${blogData.avatar?.[3]?.url}' --porcelain`
-         },
-         {
-            "step": "runPHP",
-            "code": `<?php
+	return {
+		$schema: 'https://playground.wordpress.net/blueprint-schema.json',
+		preferredVersions: {
+			php: '8.3',
+			wp: 'latest',
+		},
+		features: {
+			networking: true,
+		},
+		landingPage: '/',
+		login: true,
+		steps: [
+			{
+				step: 'mkdir',
+				path: 'wordpress/wp-content/mu-plugins',
+			},
+			{
+				step: 'writeFile',
+				path: 'wordpress/wp-content/mu-plugins/addFilter-0.php',
+				data: "<?php add_action( 'requests-requests.before_request', function( &$url ) {\n$url = 'https://playground.wordpress.net/cors-proxy.php?' . $url;\n} );",
+			},
+			{
+				step: 'installPlugin',
+				pluginData: {
+					resource: 'url',
+					url: 'https://github-proxy.com/proxy/?repo=Automattic/tumblr-theme-translator&release=v0.1.10&asset=tumblr-theme-translator.zip',
+				},
+			},
+			{
+				step: 'login',
+				username: 'admin',
+				password: 'password',
+			},
+			{
+				step: 'setSiteOptions',
+				options: {
+					template: 'tumblr3',
+					stylesheet: 'tumblr3',
+				},
+			},
+			{
+				step: 'wp-cli',
+				command: `wp media import '${blogData.avatar?.[3]?.url}' --porcelain`,
+			},
+			{
+				step: 'runPHP',
+				code: `<?php
                 $consumer = '${consumer.key}';
                 $blog_data = json_decode('${JSON.stringify(blogData)}', true);
                 $default_params = json_decode('${JSON.stringify(defaultParams)}', true);
@@ -76,8 +77,8 @@ EOD;
                 ${readFile('clear-posts.php')}
                 ${readFile('redvelet.php')}
                 ${readFile('load-posts.php')}
-            `
-         },
-      ]
-   };
+            `,
+			},
+		],
+	};
 };
